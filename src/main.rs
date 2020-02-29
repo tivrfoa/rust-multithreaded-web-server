3478f5043d4dc932a070e5b8cc616e61f019486b
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-02-29 14:54:04 -0300

    Compiler-driven development =D use of undeclared type or module `ThreadPool`

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,14 @@ const ROUTE_GET_SLEEP: &[u8] = b"GET /sleep HTTP/1.1\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
         println!("Connection established!");
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream); 
         });
     }
