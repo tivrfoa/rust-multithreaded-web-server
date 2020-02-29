cfe723c468864d0ec27298fb563eb417d10b7626
Leandro Coutinho <lescoutinhovr@hotmail.com>
2020-02-29 12:21:22 -0300

    Validating the Request and Selectively Responding

--- src/main.rs
+++ src/main.rs
@@ -21,11 +21,24 @@ fn main() {
 
         println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-        let contents = fs::read_to_string("hello.html").unwrap();
+        // byte string
+        let get = b"GET / HTTP/1.1\r\n";
 
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        if buffer.starts_with(get) {
+            let contents = fs::read_to_string("hello.html").unwrap();
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+            let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        } else {
+            let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+            let contents = fs::read_to_string("404.html").unwrap();
+
+            let response = format!("{}{}", status_line, contents);
+
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
     }
 }
